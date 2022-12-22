2a98dcea1f6c54425df2a19aec2583fd56788acd
DESKTOP-97ERRGR\Walera <aluschta17@gmail.com>
2022-12-22 09:38:56 +0200

    Create PlayerMovement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,25 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector3 target = hit.point;
+                target.y = transform.position.y;
+                agent.SetDestination(target);
+            }
         }
     }
 }
