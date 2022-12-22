176d8504cfc517b27eedba46a14f0aa44c94c50c
DESKTOP-97ERRGR\Walera <aluschta17@gmail.com>
2022-12-22 09:18:31 +0200

    Create Game Scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
